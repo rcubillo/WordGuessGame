ffafab5eed1871e3888ff95f732461137e3a619c
Chino <ronaldcubillo92@gmail.com>
2019-02-11 13:22:19 -0600

    working on the compare letters part

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,18 +4,24 @@ var lives = 15;
 var losses = 0;
 var string;
 var answerArray;
+var numBlanks = 0;
+
 
 
 // to store the letters that we already guessed
-var guessedletters = [];
+var underscore = [];
 var correctwords = [];
 var wrongword = [];
 
+//DOM
+var wordunderscore =  document.getElementById("word");
+
+
+
 //array wiht a list of words
 var words = [
-    "javascript",
-    "hello",
-    "hangman",
+    "index",
+    "sky",
     "computer",
     "chino"
 ];
@@ -29,32 +35,43 @@ console.log(choosenwords);
 //this function will change the letter to underscore depending of the lenght of the word.
 function starting() {
     for (var i = 0; i < choosenwords.length; i++) {
-        guessedletters.push(' _ ');
+        underscore.push(' _ ');
+        document.getElementById('guessed').innerHTML= underscore;
     }
-    return guessedletters;
+
+    document.getElementById('guessed').innerHTML= underscore.join(' ');
+    return underscore;
 };
-console.log(starting());
 
 
-// typing a letter and capturing into  a var (userskey).
-document.onkeyup = function(event){
-    var userskey = event.key;
 
-    // Loop so that when user types a letter it will clasify if the letter is from the choosen word it will
-    // get store in an array, if not it will be assign into another array.
-    if(choosenwords.indexOf(userskey) > -1){
-        correctwords.push(userskey);
-        console.log(correctwords);
-    } else {
-        wrongword.push(userskey)
-        console.log(wrongword);
+
+    // typing a letter and capturing into  a var (userskey).
+    document.onkeyup = function (event) {
+        var userskey = event.key;
+        // Loop so that when user types a letter it will clasify if the letter is from the choosen word it will
+        // get store in an array, if not it will be assign into another array.
+        if (choosenwords.indexOf(userskey) > -1) {
+            // adding the correct letters to the "correct array"e
+            correctwords.push(userskey);
+            // this will replace the undercore with the correct letter.
+            underscore[choosenwords.indexOf(userskey)] = userskey;
+            if (underscore.join('') == choosenwords) {
+
+                alert("You win");
+            }
+            console.log(underscore);
+
+            //console.log(correctwords);
+        } else {
+            //adding the wrong letters to the "wrong array"
+            wrongword.push(userskey)
+            console.log(wrongword);
+        }
     }
-}
 
 
-
-
-
-
-
-
+//showing the undercore word into the dom
+window.onload = function loadword(){
+    document.getElementById("word").innerHTML = starting();
+    };
